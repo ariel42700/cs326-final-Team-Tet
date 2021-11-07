128fa8bcc5d993ca1c9a438e7b6fd187f1a832f1
unknown <ariel42700@gmail.com>
2021-11-06 22:14:25 -0400

    add event listeners

--- docs/mainPage.js
+++ docs/mainPage.js
@@ -1,13 +1,42 @@
 import {database} from './jobApi.js';
+function getDropdown(id) {
+    let select = document.getElementById(id);
+    let value = select.options[select.selectedIndex].value;
+    return value;
+}
+function getChecklistItem(id) {
+    let value = '';
+    let radios = document.getElementsById(id);
+    for (let i = 0, length = radios.length; i < radios.length; i++) {
+        if (radios[i].checked) {
+            // do whatever you want with the checked radio
+            value = radios[i].value;
+
+            // only one radio can be logically checked, don't check the rest
+            break;
+        }
+    }
+    return value;   
+}
 function getSearch() {
     const response = await fetch('/search', {
         method: 'POST',
-        body: JSON.stringify({
-            locationPreference: document.getElementById('').value,
-            name: cost.billname,
-            cost: cost.cost * 100,
-            contributors: cost.contributors
+        database: JSON.stringify({
+            workStudy: getChecklistItem('checklist1'),
+            locationPreference: getChecklistItem('checklist2'),
+            jobType: getDropdown('dropdown')
         })
-        window.location.replace("PopupSearch.html");
+    })
+    window.location.replace("PopupSearch.html");
+}
+function getJobPage() {
+    const response = await fetch('/jobPage')
+    window.location.replace("job_page.html");
 }
 document.getElementById('search').addEventListener('click', getSearch);
+
+let tags = document.getElementsByTagName('a');
+
+for (let i = 0; i < tags.length ; i++) {
+    Anchors[i].addEventListener("click", getJobPage);
+}
